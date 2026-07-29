d06e8a436f7011f0f6e3bebc2857fd23e3ec0cf4
José Elías <devjoseelias@gmail.com>
2026-07-28 18:06:38 -0600

    added a new function to hide the menu when the user scrolls, also added the 'hero' section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-const dropdownButton = document.querySelector("#button");
+const dropdownButton = document.querySelector("#header-button");
 const deployableMenu = document.querySelector("#deployableMenu");
-const menuIcon = document.querySelector("#button i");
+const menuIcon = document.querySelector("#header-button i");
 
 dropdownButton.addEventListener("click", () => {
     deployableMenu.classList.toggle("active");
     menuIcon.classList.toggle('fa-bars');
     menuIcon.classList.toggle('fa-x');
+});
+window.addEventListener("scroll", () => {
+    if(deployableMenu.classList.contains("active")){
+        deployableMenu.classList.remove("active");
+        menuIcon.classList.toggle('fa-bars');
+        menuIcon.classList.toggle('fa-x');
+    }
 });
