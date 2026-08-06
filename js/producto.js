56228e111b91f1a102cc49c7215dcf100d370509
José Elías <devjoseelias@gmail.com>
2026-08-06 17:13:48 -0600

    haciendo el JS, corrigiendo errores, modularizando 😭

---
+++ js/producto.js
@@ -0,0 +1,19 @@
+/*
+            <h2 class="nombre-producto">Crema aclaradora de noche</h2>
+            <div class="contenedor-info">
+                <div class="contenedor-imagen">
+                    <img src="../assets/Images/products/crema_aclaradora_de_noche.webp" alt="" />
+                </div>
+                <div class="contenedor-texto">
+                    <h3 class="descripcion">Descripción: </h3>
+                    <p class="descripcion-producto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit accusantium voluptas unde vitae corrupti, ipsum tenetur facilis maxime obcaecati laudantium alias quam, asperiores quidem ad quo tempora officiis assumenda numquam.</p>
+                    <p class="precio-producto">$9999.99MXN</p>
+                    <button class="carrito">
+                        <i class="fa-solid fa-cart-shopping"></i> Agregar al carrito
+                    </button>
+                    <button class="comprar">
+                        <i class="fa-solid fa-bag-shopping"></i> Comprar ahora
+                    </button>
+                </div>
+            </div>
+*/
